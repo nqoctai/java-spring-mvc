21e158a01c9ccd9e1ab2acc76a684348de54ad79
nqoctai <jeweltai1210@gmail.com>
2024-03-14 17:24:16 +0700

    video43

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hoi Dan IT";
+        return "Hello World with Spring Boot!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can acsess this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can acsess this page";
     }
 }
