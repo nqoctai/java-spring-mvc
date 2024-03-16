780f6ddc2b624422d55ab6b94d8bf8260da42208
nqoctai <jeweltai1210@gmail.com>
2024-03-16 22:02:23 +0700

    video53

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,16 +1,22 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+
     private UserService userService;
 
     public UserController(UserService userService) {
@@ -24,18 +30,32 @@ public class UserController {
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
+    }
+
 }
 
 // @RestController
 // public class UserController {
 
+// // DI : dependency injection
 // private UserService userService;
 
 // public UserController(UserService userService) {
 // this.userService = userService;
 // }
 
-// @GetMapping()
+// @GetMapping("")
 // public String getHomePage() {
 // return this.userService.handleHello();
 // }
